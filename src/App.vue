8e1222ea427c820c1e293d063f540cb47e209925
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-03-09 00:32:08 +0100

    created components for menu.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainMenu />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from './components/MainMenu.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+
+    MainMenu,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/StructureItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <img
+      src="https://s3.amazonaws.com/hr-assets/0/1527626183-88c8070977-isitBSTSample0.png"
+      alt="Binary Search"
+    ><span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StructureItem',
+  props: {
+    name: String,
+    imageURL: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #c7733b;
+}
+</style>
